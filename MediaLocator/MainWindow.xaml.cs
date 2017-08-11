dd1ba3263fb87684fb7ef619899208e3af515744
Norbert Lugosi <lugosi.norbert@hotmail.com>
2017-08-11 08:56:02 +0200

    comit

--- MediaLocator/MainWindow.xaml.cs
+++ MediaLocator/MainWindow.xaml.cs
@@ -20,10 +20,11 @@ using System.Collections;
 using MediaLocator.view;
 using System.IO;
 using MediaLocator.logging;
-
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Windows.Forms;
 using static System.Windows.Forms.DataFormats;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+using ProgressBar = System.Windows.Controls.ProgressBar;
 
 namespace MediaLocator
 {
@@ -41,7 +42,6 @@ namespace MediaLocator
         private static List<FileModification> fileModificationList = new List<FileModification>();
 
 
-
         public MainWindow()
         {
             InitializeComponent();
@@ -54,15 +54,28 @@ namespace MediaLocator
             ListFiles.MouseDoubleClick += ListFiles_MouseDoubleClick;
             hidePlayer();
             getView = new MediaView(PalyingProgress);
+        }
 
-            
-    }
+        void HideSplitBtn()
+        {
+            concateBtn.Opacity = 0;
+            splitBtn.Opacity = 0;
+            splitTime.Opacity = 0;
+        }
+
+        void ShowSplitBtn()
+        {
+            concateBtn.Opacity = 100;
+            splitBtn.Opacity = 100;
+            splitTime.Opacity = 100;
+        }
 
         private void AlbumBtn_Click(object sender, RoutedEventArgs e)
         {
             MediaFunctions createAlbum = new MediaFunctions();
-            string input = Microsoft.VisualBasic.Interaction.InputBox("Provide your playlist name in the box filed.", "Give your playlist a name", "MyPlayList", -1, -1);
-            
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Provide your playlist name in the box filed.",
+                "Give your playlist a name", "MyPlayList", -1, -1);
+
             createAlbum.CreateM3UData(check, folderPath, input);
         }
 
@@ -86,16 +99,13 @@ namespace MediaLocator
                     ShowSplitBtn();
                     mp3FilePath = fullPath;
                     FillIDList();
-
                 }
                 foreach (string item in FolderBrowser.getMusics())
                 {
                     if (item.Equals(ex.ToUpper()))
                     {
                         showPlayer();
-
                     }
-
                 }
                 foreach (string item in FolderBrowser.getVideos())
                 {
@@ -111,7 +121,6 @@ namespace MediaLocator
                 if (!System.IO.Path.GetExtension(fullPath).Equals(".mp3"))
                 {
                     HideSplitBtn();
-
                 }
             }
             catch (NullReferenceException nre)
@@ -122,9 +131,6 @@ namespace MediaLocator
             {
                 Console.WriteLine("Error: " + allE.Message);
             }
-            
-            
-
         }
 
         private void VideoBtn_Click(object sender, RoutedEventArgs e)
@@ -132,7 +138,7 @@ namespace MediaLocator
             ListFiles.Items.Clear();
             foreach (var file in FolderBrowser.GetFilteredList(FolderBrowser.getVideos(), folderPath, folderPath))
             {
-                ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
+                ListFiles.Items.Add(new ListviewText {Name = file.ToString()});
             }
         }
 
@@ -143,19 +149,13 @@ namespace MediaLocator
             {
                 foreach (var file in FolderBrowser.GetFilteredList(FolderBrowser.getMusics(), folderPath, folderPath))
                 {
-                    ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
+                    ListFiles.Items.Add(new ListviewText {Name = file.ToString()});
                 }
             }
             catch (NullReferenceException nre)
             {
                 Console.WriteLine(nre.Message);
-                
             }
-
-         
-
-
-
         }
 
         private void PictureBtn_Click(object sender, RoutedEventArgs e)
@@ -163,7 +163,7 @@ namespace MediaLocator
             ListFiles.Items.Clear();
             foreach (var file in FolderBrowser.GetFilteredList(FolderBrowser.getPictures(), folderPath, folderPath))
             {
-                ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
+                ListFiles.Items.Add(new ListviewText {Name = file.ToString()});
             }
         }
 
@@ -180,7 +180,6 @@ namespace MediaLocator
             if (clicks % 2 == 0)
             {
                 ShowHideMenu("sbHideBottomMenu", NavPanel);
-
             }
             else
             {
@@ -190,12 +189,10 @@ namespace MediaLocator
         }
 
 
-
         private void ShowHideMenu(string Storyboard, StackPanel pnl)
         {
             Storyboard sb = Resources[Storyboard] as Storyboard;
             sb.Begin(pnl);
-
         }
 
         private void hidePlayer()
@@ -227,27 +224,28 @@ namespace MediaLocator
             splitTime.Opacity = 100;
             fullScrnBtn.Opacity = 100;
         }
+
         private void ShowIDTagBox()
         {
             IDTagBox.Opacity = 100;
-           
         }
+
         private void HideIDTagBox()
         {
             IDTagBox.Opacity = 0;
-
         }
+
         private void SplitBtn_Click(object sender, RoutedEventArgs e)
         {
-            FileModification fileModification = new FileModification(DateTime.Now, mp3FilePath, ".mp3", "Split interval: " + splitTime.Text);
+            FileModification fileModification =
+                new FileModification(DateTime.Now, mp3FilePath, ".mp3", "Split interval: " + splitTime.Text);
             fileModification.addModificationToList(fileModificationList);
             Logging.AppendToLogFile(fileModificationList);
             MediaFunctions splitter = new MediaFunctions();
-            var selectedStockObject = ListFiles.SelectedItem as ListviewText;       
+            var selectedStockObject = ListFiles.SelectedItem as ListviewText;
             string filename = selectedStockObject.ToString();
             string fullPath = folderPath + @"\" + filename;
             splitter.SplitMp3(fullPath, Convert.ToInt32(splitTime.Text));
-            
         }
 
         private void StopBtn_Click(object sender, RoutedEventArgs e)
@@ -263,7 +261,7 @@ namespace MediaLocator
         private void PlayBtn_Click(object sender, RoutedEventArgs e)
         {
             getView.playMedia();
-            
+
             Console.WriteLine(getView.mediaElement.Position);
         }
 
@@ -281,18 +279,18 @@ namespace MediaLocator
             ListFiles.Items.Clear();
             foreach (var file in FolderBrowser.getFileList(folderPath, folderPath))
             {
-                ListFiles.Items.Add(new ListviewText { Name = file.ToString()});
-                
+                ListFiles.Items.Add(new ListviewText {Name = file.ToString()});
             }
         }
+
         public class ListviewText
         {
             public string Name { get; set; }
+
             public override string ToString()
             {
                 return Name;
             }
-
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
@@ -304,43 +302,45 @@ namespace MediaLocator
             {
                 var selectedStockObject = ListFiles.SelectedItems[i] as ListviewText;
                 check.Add(folderPath + "\\" + selectedStockObject);
-                
             }
-
-
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
         private void IDTagBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
-        
-        private void IDTagBox_ItemDoubleClicked(object sender,MouseButtonEventArgs e)
-        {
 
+        private void IDTagBox_ItemDoubleClicked(object sender, MouseButtonEventArgs e)
+        {
             try
             {
                 MediaView.CloseMedia();
                 if (IDTagBox.SelectedIndex == 0)
                 {
-                    MediaFunctions.SetTitle(mp3FilePath, Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?", "Title", IDTagBox.Items.GetItemAt(0).ToString()));
+                    MediaFunctions.SetTitle(mp3FilePath,
+                        Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?",
+                            "Title", IDTagBox.Items.GetItemAt(0).ToString()));
                 }
                 if (IDTagBox.SelectedIndex == 1)
                 {
-                    MediaFunctions.SetArtist(mp3FilePath, Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?", "Artist", IDTagBox.Items.GetItemAt(1).ToString()));
+                    MediaFunctions.SetArtist(mp3FilePath,
+                        Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?",
+                            "Artist", IDTagBox.Items.GetItemAt(1).ToString()));
                 }
                 if (IDTagBox.SelectedIndex == 2)
                 {
-                    MediaFunctions.SetAlbum(mp3FilePath, Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?", "Album", IDTagBox.Items.GetItemAt(2).ToString()));
+                    MediaFunctions.SetAlbum(mp3FilePath,
+                        Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?",
+                            "Album", IDTagBox.Items.GetItemAt(2).ToString()));
                 }
                 if (IDTagBox.SelectedIndex == 3)
                 {
-                    string yearString = Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?", "Year", (IDTagBox.Items.GetItemAt(3).ToString()));
+                    string yearString =
+                        Microsoft.VisualBasic.Interaction.InputBox("Please enter the value you wish to assign?", "Year",
+                            (IDTagBox.Items.GetItemAt(3).ToString()));
                     short yearShort = short.Parse(yearString);
                     MediaFunctions.SetYear(mp3FilePath, yearShort);
                 }
@@ -349,66 +349,26 @@ namespace MediaLocator
                     System.Windows.MessageBox.Show("Unmodifiable parameter :( !");
                 }
                 FillIDList();
-        
             }
-            catch(Exception) { }
-
-
+            catch (Exception)
+            {
+            }
         }
+
         private void FillIDList()
         {
             hidePlayer();
             IDTagBox.Items.Clear();
             ShowIDTagBox();
-        
-           
+
+
             foreach (String idTag in MediaFunctions.ViewMp3Tags(mp3FilePath))
             {
                 Console.WriteLine(idTag);
                 IDTagBox.Items.Add(idTag);
+            }
 
-
-        }
-
-        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var position = getView.mediaElement.Position;
-
-            moviePosition = position;
-            Console.WriteLine(getView.mediaElement.Position.ToString());
-            MediaView mv = new MediaView(new ProgressBar());
-            Window1 w1 = new Window1();
-
-            var selectedStockObject = ListFiles.SelectedItem as ListviewText;
-
-            w1.WindowStyle = WindowStyle.None;
-            w1.ResizeMode = ResizeMode.NoResize;
-            w1.Left = 0;
-            w1.Top = 0;
-            w1.Width = SystemParameters.VirtualScreenWidth;
-            w1.Height = SystemParameters.VirtualScreenHeight;
-            w1.Topmost = true;
-            w1.fullscrnPanel.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
-            w1.fullscrnPanel.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
-
-            string filename = selectedStockObject.ToString();
-            string fullPath = folderPath + @"\" + filename;
-            w1.Show();
-
-            mv.getMedia(fullPath, w1.fullscrnPanel);
-            mv.mediaElement.Position = moviePosition;
-            mv.playMedia();
-
-            
-
-
-
-
-        }
-
-        private void MetroWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.F5)
+            void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
             {
                 var position = getView.mediaElement.Position;
 
@@ -437,81 +397,94 @@ namespace MediaLocator
                 mv.mediaElement.Position = moviePosition;
                 mv.playMedia();
             }
-        }
 
-       private void HideSplitBtn()
-        {
-            concateBtn.Opacity = 0;
-            splitBtn.Opacity = 0;
-            splitTime.Opacity = 0;
-        }
-       
-       private void ShowSplitBtn()
-        {
-            concateBtn.Opacity = 100;
-            splitBtn.Opacity = 100;
-            splitTime.Opacity = 100;
-        }
-
-        private void concateBtn_Click(object sender, RoutedEventArgs e)
-        {
-            FileModification fileModification = new FileModification(DateTime.Now, mp3FilePath, ".mp3", "concate");
-            fileModification.addModificationToList(fileModificationList);
-            Logging.AppendToLogFile(fileModificationList);
-            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
-            if (dialog.ShowDialog(this).GetValueOrDefault())
+            void MetroWindow_KeyDown(object sender, KeyEventArgs e)
             {
-                folderPath = dialog.SelectedPath;
+                if (e.Key == Key.F5)
+                {
+                    var position = getView.mediaElement.Position;
+
+                    moviePosition = position;
+                    Console.WriteLine(getView.mediaElement.Position.ToString());
+                    MediaView mv = new MediaView(new ProgressBar());
+                    Window1 w1 = new Window1();
+
+                    var selectedStockObject = ListFiles.SelectedItem as ListviewText;
+
+                    w1.WindowStyle = WindowStyle.None;
+                    w1.ResizeMode = ResizeMode.NoResize;
+                    w1.Left = 0;
+                    w1.Top = 0;
+                    w1.Width = SystemParameters.VirtualScreenWidth;
+                    w1.Height = SystemParameters.VirtualScreenHeight;
+                    w1.Topmost = true;
+                    w1.fullscrnPanel.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+                    w1.fullscrnPanel.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+
+                    string filename = selectedStockObject.ToString();
+                    string fullPath = folderPath + @"\" + filename;
+                    w1.Show();
+
+                    mv.getMedia(fullPath, w1.fullscrnPanel);
+                    mv.mediaElement.Position = moviePosition;
+                    mv.playMedia();
+                }
             }
-        }
 
 
 
 
 
-
-
-
-        /*public void watchFolder()
-        {
-            
-            FileSystemWatcher Watcher = new FileSystemWatcher();
-            Watcher.Path = folderPath + @"\";
-            Watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
-               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-            Watcher.EnableRaisingEvents = true;
-
-            Watcher.Renamed += new RenamedEventHandler(Watcher_Renamed);
-            Watcher.Changed += new FileSystemEventHandler(Watcher_OnChanged);
-            Watcher.Created += new FileSystemEventHandler(Watcher_OnChanged);
-            Watcher.Deleted += new FileSystemEventHandler(Watcher_OnChanged);
-
-
-        }
-
-        private void Watcher_OnChanged(object sender, FileSystemEventArgs e)
-        {
-            ListFiles.Items.Clear();
-            foreach (var file in FolderBrowser.getFileList(folderPath, folderPath))
+            void concateBtn_Click(object sender, RoutedEventArgs e)
             {
-                ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
-
+                FileModification fileModification = new FileModification(DateTime.Now, mp3FilePath, ".mp3", "concate");
+                fileModification.addModificationToList(fileModificationList);
+                Logging.AppendToLogFile(fileModificationList);
+                var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+                if (dialog.ShowDialog(this).GetValueOrDefault())
+                {
+                    folderPath = dialog.SelectedPath;
+                }
             }
-        }
 
-        private void Watcher_Renamed(object sender, RenamedEventArgs e)
-        {
-            ListFiles.Items.Clear();
-            foreach (var file in FolderBrowser.getFileList(folderPath, folderPath))
+
+            /*public void watchFolder()
             {
-                ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
-
+                
+                FileSystemWatcher Watcher = new FileSystemWatcher();
+                Watcher.Path = folderPath + @"\";
+                Watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                Watcher.EnableRaisingEvents = true;
+    
+                Watcher.Renamed += new RenamedEventHandler(Watcher_Renamed);
+                Watcher.Changed += new FileSystemEventHandler(Watcher_OnChanged);
+                Watcher.Created += new FileSystemEventHandler(Watcher_OnChanged);
+                Watcher.Deleted += new FileSystemEventHandler(Watcher_OnChanged);
+    
+    
             }
+    
+            private void Watcher_OnChanged(object sender, FileSystemEventArgs e)
+            {
+                ListFiles.Items.Clear();
+                foreach (var file in FolderBrowser.getFileList(folderPath, folderPath))
+                {
+                    ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
+    
+                }
+            }
+    
+            private void Watcher_Renamed(object sender, RenamedEventArgs e)
+            {
+                ListFiles.Items.Clear();
+                foreach (var file in FolderBrowser.getFileList(folderPath, folderPath))
+                {
+                    ListFiles.Items.Add(new ListviewText { Name = file.ToString() });
+    
+                }
+            }
+            */
         }
-        */
-
-
     }
-
-
 }
